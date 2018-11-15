1b881a136d2b08b42868807d4fcc2528bba98126
Sayyid Khan <sayyidkhan92@hotmail.com>
2018-11-15 19:01:52 +0800

    practising switches

---
+++ java practise projects/project_1/Switches.java
@@ -0,0 +1,12 @@
+public class Switches{
+    public static void main(String[] args) {
+    
+        char data = 'a';
+    
+        switch(data){
+          case 'a': System.out.println('a');break;
+          case 'b': System.out.println('b');break;
+          default: System.out.println("no such value");break;
+        } 
+      }    
+}
