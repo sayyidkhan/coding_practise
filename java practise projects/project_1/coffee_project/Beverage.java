93ab5a06bc53f819b21df4b884f8bd3ce662836d
Sayyid Khan <sayyidkhan92@hotmail.com>
2018-11-16 00:15:33 +0800

    update data

---
+++ java practise projects/project_1/coffee_project/Beverage.java
@@ -0,0 +1,18 @@
+public class Beverage {
+	
+	
+	public Beverage() {
+
+	}
+
+	public void isFull() {
+
+		System.out.println("Your order is full!");
+
+	}
+
+	public static void main(String[] args) {
+    
+	}
+
+}
