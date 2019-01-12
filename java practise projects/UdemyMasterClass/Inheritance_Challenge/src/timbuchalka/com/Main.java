e863c9dff5d4c36d043a531bc214305630dce84e
Sayyid Khan <sayyidkhan92@hotmail.com>
2019-01-12 17:53:12 +0800

    working on the inheritance project
    Signed-off-by: Sayyid Khan <sayyidkhan92@hotmail.com>

--- java practise projects/UdemyMasterClass/Inheritance_Challenge/src/timbuchalka/com/Main.java
+++ java practise projects/UdemyMasterClass/Inheritance_Challenge/src/timbuchalka/com/Main.java
@@ -1,4 +1,17 @@
 package timbuchalka.com;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Outlander outlander = new Outlander(36);
+        outlander.steer(45);
+        outlander.accelerate(30);
+        outlander.accelerate(20);
+        outlander.accelerate(-42);
+
+    }
+
+
+
 }
