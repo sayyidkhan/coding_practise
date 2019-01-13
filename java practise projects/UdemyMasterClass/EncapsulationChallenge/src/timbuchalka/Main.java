c74cf336b5808a1929f7748a55cf6a71d1d053c3
Sayyid Khan <sayyidkhan92@hotmail.com>
2019-01-13 11:39:30 +0800

    worked on a printer project

--- java practise projects/UdemyMasterClass/EncapsulationChallenge/src/timbuchalka/Main.java
+++ java practise projects/UdemyMasterClass/EncapsulationChallenge/src/timbuchalka/Main.java
@@ -1,4 +1,18 @@
 package timbuchalka;
 
 public class Main {
+
+    public static void main(String[] args) {
+        Printer printer = new Printer(50,false);
+        System.out.println("initial page count = " + printer.getPagesPrinted());
+        int pagesPrinted = printer.printPages(4);
+        System.out.println("Pages Printed was " + pagesPrinted + ". new total print count for printer = " +
+                printer.getPagesPrinted());
+        pagesPrinted = printer.printPages(2);
+        System.out.println("Pages Printed was " + pagesPrinted + ". new total print count for printer = " +
+                printer.getPagesPrinted());
+
+
+    }
+
 }
