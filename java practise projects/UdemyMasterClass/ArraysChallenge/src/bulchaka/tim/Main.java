8b288d1636f373f5b9f6e043726ac9a0515d5678
Sayyid Khan <sayyidkhan92@hotmail.com>
2019-02-20 09:32:45 +0800

    Update Main.java

--- java practise projects/UdemyMasterClass/ArraysChallenge/src/bulchaka/tim/Main.java
+++ java practise projects/UdemyMasterClass/ArraysChallenge/src/bulchaka/tim/Main.java
@@ -1,6 +1,6 @@
 package bulchaka.tim;
+import java.util.Arrays;
 import java.util.Scanner;
-
 
 public class Main {
 
@@ -8,28 +8,38 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] myIntegers = getIntegers(5);
+        int[] myIntegers = getIntegers(5); // call for the array
+        int[] sorted = sortIntgers(myIntegers);
+        printArray(sorted);
+
+        System.out.println("#################################");
+
+        int[] sorted2 = copyArray(sorted);
+        printArray(sorted2);
 
     }
 
     public static int[] getIntegers(int capacity){
-        int[] array = new int[capacity];
+        int[] array = new int[capacity]; // create a new array based on the size of the integer
         System.out.println("Enter " + capacity + " integer values:\r");
         for(int i =0; i < array.length;i++) {
-            array[i] = scanner.nextInt();
+            array[i] = scanner.nextInt(); // input integer on every loop
         }
-        return array;
+        return array; // returns an int array
     }
 
     public static void printArray(int[] array) {
         for(int i = 0; i <array.length;i++){
             System.out.println("Element " + i + " contents " + array[i]);
+            // runs a for loop to return the contents of the array
         }
     }
 
-    public static void sortIntgers(int[] array) {
+    public static int[] sortIntgers(int[] array) {
+        // initalize a new array based on the array length of the previous array
         int[] sortedArray = new int[array.length];
         for(int i = 0;i < array.length; i++){
+            // put all the existing array and make a copy into the new array
             sortedArray[i] = array[i];
         }
 
@@ -37,13 +47,24 @@ public class Main {
         int temp;
         while(flag) {
             flag = false;
-            for(int j = 0;j < sortedArray.length -1 ; j++){
+            for(int j = 0;j < (sortedArray.length -1) ; j++){
                 if(sortedArray[j] < sortedArray[j + 1]){
-
+                    temp = sortedArray[j];
+                    sortedArray[j] = sortedArray[j+1];
+                    sortedArray[j+1] = temp;
+                    flag = true;
                 }
             }
+
         }
-        //
+        return sortedArray;
+    }
+
+
+    public static int[] copyArray(int[] array){
+        int[] sortedArray = Arrays.copyOf(array,array.length);
+        sortedArray[4] = 50;
+        return sortedArray;
     }
 
 
