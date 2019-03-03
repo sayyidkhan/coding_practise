8935a612427b6d275eb05d0c279859a92144d573
Sayyid Khan <sayyidkhan92@hotmail.com>
2019-03-03 15:39:24 +0800

    done for the arraylistchallenge
    Signed-off-by: Sayyid Khan <sayyidkhan92@hotmail.com>

--- java practise projects/UdemyMasterClass/ArrayListChallenge/src/com/xaiver/Contact.java
+++ java practise projects/UdemyMasterClass/ArrayListChallenge/src/com/xaiver/Contact.java
@@ -3,39 +3,54 @@ package com.xaiver;
 import java.util.ArrayList;
 
 
-public class Contact {
+ class Contact {
 
     private String name;
     private int phoneNumber;
 
-    public Contact(String name, int phoneNumber) {
+    Contact(String name, int phoneNumber) {
         this.name = name;
         this.phoneNumber = phoneNumber;
     }
 
-    public static int validateObjectArrayIndex(String queryname,ArrayList<Contact> contactListArray){
+    // method overloading with contactName
+    static void modifyObjectData(String existingContactName,String newContactName,ArrayList<Contact> contactListArray){
+        Contact contactObject = accessObject(existingContactName,contactListArray); // access the object via a method
+        contactObject.setName(newContactName); // change the object via the setName
+        System.out.println("Contact name have been been changed ");
+        System.out.println("from: " + existingContactName);
+        System.out.println("to: " + contactObject.getName());
+    }
+
+    // method overloading with contactNumber
+    static void modifyObjectData(String contactName,int newContactNumber,ArrayList<Contact> contactListArray){
+        Contact contactObject = accessObject(contactName,contactListArray); // access the object via a method
+        int currentContactNumber = contactObject.getPhoneNumber();
+        contactObject.setPhoneNumber(newContactNumber); // change the object via the setName
+        System.out.println("Contact number have been been changed from: " + currentContactNumber +  " to : " + contactObject.getPhoneNumber());
+    }
+
+    private static Contact accessObject(String contactName,ArrayList<Contact> contactListArray){
+        int positionToChangeNumber = validateObjectArrayIndex(contactName,contactListArray);
+        return contactListArray.get(positionToChangeNumber); // return the individual contact object
+    }
+
+    static int validateObjectArrayIndex(String queryName,ArrayList<Contact> contactListArray){
         for(int i = 0; i < contactListArray.size(); i++ ) {
             String currentName = contactListArray.get(i).getName();
-            if(queryname.equals(currentName)){
+            if(queryName.equals(currentName)){
                 return i;
             }
         }
         return -1;
     }
 
-    public static boolean queryName(String queryname,ArrayList<Contact> contactListArray){
+    static boolean queryName(String queryname,ArrayList<Contact> contactListArray){
         int state = validateObjectArrayIndex(queryname,contactListArray);
-        if(state >= 0){
-            return !true;
-        }
-            return !false;
+        return (state < 0);
     }
 
-    public static void modifyContact(){
-
-    }
-
-    public static void printContact(String queryname,ArrayList<Contact> contactListArray){
+    static void printContact(String queryname,ArrayList<Contact> contactListArray){
         int state = validateObjectArrayIndex(queryname,contactListArray);
         if(state >= 0) {
             System.out.println("Contact found.");
@@ -44,6 +59,16 @@ public class Contact {
         }
         else {
             System.out.println("Unable to find " + queryname + " in the contact book.");
+        }
+    }
+
+    static void showAllContacts(ArrayList<Contact> contactListArray){
+        for(int i = 0;i < contactListArray.size();i++){
+            System.out.println();
+            System.out.println("------- " + (i+1) + " of " +  contactListArray.size() + " -------");
+            System.out.println("Contact Name: " + contactListArray.get(i).getName());
+            System.out.println("Contact Number: " + contactListArray.get(i).getPhoneNumber());
+            System.out.println();
         }
     }
 
--- java practise projects/UdemyMasterClass/ArrayListChallenge/src/com/xaiver/MobilePhone.java
+++ java practise projects/UdemyMasterClass/ArrayListChallenge/src/com/xaiver/MobilePhone.java
@@ -1,34 +1,28 @@
 package com.xaiver;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
-public class MobilePhone {
+class MobilePhone {
 
     // to store mobile phone options
     private ArrayList<Contact> contactList;
 
-    public MobilePhone() {
+    MobilePhone() {
         ArrayList<Contact> contactList = new ArrayList<Contact>();
         this.contactList = contactList;
         this.contactList.add(new Contact("apple",8765432));
     }
 
-    public int contactListLength() {
-        return this.contactList.size();
-    }
-
     // does validation to check whether contact exists
     private boolean queryNameValidation(String contactName){
-        if(Contact.queryName(contactName,this.contactList)){
-            return true;
-        }
-        return false;
+        return Contact.queryName(contactName,this.contactList);
     }
 
-    public void store(String contactName,int contactNumber){
+    private void store(String contactName,int contactNumber){
         if(queryNameValidation(contactName)){
             // if return true, means does not contain contactName, accept
-            // if false, means does not contain contactName, reject
+            // if false, means contactName exists, reject
 
             // add a new contract into the person mobile phone
             this.contactList.add(new Contact(contactName, contactNumber));
@@ -41,11 +35,11 @@ public class MobilePhone {
         }
     }
 
-    public void searchContacts(String contactName){
+    private void searchContacts(String contactName){
         Contact.printContact(contactName,this.contactList);
     }
 
-    public void remove(String contactName){
+    private void remove(String contactName){
         if(!queryNameValidation(contactName)){
             // if return true, does not have contactName, we want to reject
             // if return false, means contact contains, we want to accept
@@ -58,10 +52,153 @@ public class MobilePhone {
         }
     }
 
-
-    public void modify(){
-
+    private void modify(String contactName,String newContactName){
+        // print out existing info, before changing the state
+        if(queryNameValidation(contactName)){
+            // if name does not exist, end method
+            System.out.println("Contact Name " + contactName  + " does not exist");
+        }
+        else{
+            // if name exists, continue method
+            Contact.modifyObjectData(contactName,newContactName,this.contactList);
+        }
     }
 
+    private void modify(String contactName,int newContactNumber){
+        // print out existing info, before changing the state
+        if(queryNameValidation(contactName)){
+            // if name does not exist, end method
+            System.out.println("Contact Name " + contactName  + " does not exist");
+        }
+        else{
+            // if name exists, continue method
+            Contact.modifyObjectData(contactName,newContactNumber,this.contactList);
+        }
+    }
+
+    private void modifyMenu(){
+        System.out.println("############################");
+        System.out.println("1. Modify the contact name");
+        System.out.println("2. Modify the contact number");
+        System.out.println("############################");
+    }
+
+    private void modify(){
+        int modifyMenuOptions = 1; // init a number to let the default menu run, number can be anything
+        Scanner readModifyMenuOptions = new Scanner(System.in);
+        Scanner readModifyMenuDetails = new Scanner(System.in);
+        while(modifyMenuOptions != 0) {
+            modifyMenu();
+            System.out.println("Enter an option: ");
+            modifyMenuOptions = readModifyMenuOptions.nextInt();
+            readModifyMenuOptions.nextLine(); // catch enter statement, escaped characters
+            switch (modifyMenuOptions){
+                case 1:
+                    System.out.println("1. Modify the contact name");
+                    System.out.println("Enter an existing contact name: ");
+                    String existingContactName = readModifyMenuDetails.nextLine();
+                    System.out.println("Enter a new contact name: ");
+                    String newContactName = readModifyMenuDetails.nextLine();
+                    modify(existingContactName,newContactName); // change the contact name details
+                    modifyMenuOptions = 0; // stop the loop
+                    break;
+                case 2:
+                    System.out.println("2. Modify the contact number");
+                    System.out.println("Enter an existing contact Name: ");
+                    String contactName = readModifyMenuDetails.nextLine();
+                    System.out.println("Enter a new contact number: ");
+                    int newContactNumber = readModifyMenuDetails.nextInt();
+                    modify(contactName,newContactNumber); // change the mobile number details
+                    modifyMenuOptions = 0; // stop the loop
+                    break;
+                default:
+                    System.out.println("--- invalid option ---");
+            }
+        }
+    }
+
+    private void showAllContacts(){
+        if(this.contactList.size() == 0){
+            System.out.println();
+            System.out.println("--- no contacts in the contact book ---");
+            System.out.println();
+        }
+        else{
+            Contact.showAllContacts(this.contactList);
+        }
+    }
+
+    private static void printMenuItems(){
+        System.out.println("######################################");
+        System.out.println("--- Mobile Main Menu ---");
+        System.out.println("1. Add New Contacts ");
+        System.out.println("2. Modify an existing contacts");
+        System.out.println("3. Remove an existing Contact");
+        System.out.println("4. Search for an existing contact name");
+        System.out.println("5. Show all contacts in the phone book");
+        System.out.println("0. exit the program");
+        System.out.println("######################################");
+    }
+
+    void menu(){
+        Scanner readMenuOptions = new Scanner(System.in);
+        Scanner readMenuDetails = new Scanner(System.in);
+        int optionsMenu = 1; // init a number to let the default menu run, number can be anything
+
+        while(optionsMenu != 0) {
+            printMenuItems();
+            System.out.println("Enter an option: ");
+            optionsMenu = readMenuOptions.nextInt();
+            readMenuOptions.nextLine(); // catch enter statement, escaped characters
+            switch (optionsMenu) {
+                // add new contacts
+                case 1:
+                    System.out.println("1. Add New Contacts ");
+                    ////////////////////// store contacts //////////////////////////
+                    System.out.println("Enter a new contact Name: ");
+                    String contactName = readMenuDetails.nextLine();
+                    System.out.println("Enter a new 8 digits contact Number: ");
+                    int contactNumber = readMenuDetails.nextInt();
+                    readMenuDetails.nextLine(); // catch enter statement, escaped characters
+                    store(contactName,contactNumber); // add new contact
+                    ////////////////////// store contacts //////////////////////////
+                    continue;
+                case 2:
+                    System.out.println("2. Modify an existing contacts");
+                    ////////////////////// modify contacts //////////////////////////
+                    modify();
+                    ////////////////////// modify contacts //////////////////////////
+                    continue;
+                case 3:
+                    System.out.println("3. Remove an existing Contact");
+                    ////////////////////// remove contacts //////////////////////////
+                    System.out.println("Enter an contact name to delete: ");
+                    String existingContactName = readMenuDetails.nextLine();
+                    remove(existingContactName);
+                    ////////////////////// remove contacts //////////////////////////
+                    continue;
+                case 4:
+                    System.out.println("4. Search for an existing contact name");
+                    ////////////////////// search contacts //////////////////////////
+                    System.out.println("Enter an existing contact Name to be searched: ");
+                    String searchContactName = readMenuDetails.nextLine();
+                    searchContacts(searchContactName);
+                    ////////////////////// search contacts //////////////////////////
+                    continue;
+                case 5:
+                    System.out.println("5. Show all contacts in the phone book");
+                    ////////////////////// show all contacts ////////////////////////
+                    showAllContacts();
+                    ////////////////////// show all contacts ////////////////////////
+                    continue;
+                default:
+                    System.out.println("--- invalid option ---");
+                    continue;
+                case 0:
+                    System.out.println("--- Program End ---");
+                    break;
+            }
+        }
+    }
 
 }
