9ecdf170639af8b3369bfe1d3e66d77e5014acc3
Sayyid Khan <sayyidkhan92@hotmail.com>
2019-04-11 23:03:04 +0800

    learning java Generics

--- java practise projects/UdemyMasterClass/Generics/src/GenericsPart2/Main.java
+++ java practise projects/UdemyMasterClass/Generics/src/GenericsPart2/Main.java
@@ -8,12 +8,32 @@ public class Main {
         BaseballPlayer pat = new BaseballPlayer("Pat");
         SoccerPlayer beckham = new SoccerPlayer("Beckham");
 
-        Team adelaideCrows = new Team("Adelaide Crows");
+        Team<FootballPlayer> adelaideCrows = new Team<FootballPlayer>("Adelaide Crows");
         adelaideCrows.addPlayer(joe);
-        adelaideCrows.addPlayer(pat);
-        adelaideCrows.addPlayer(beckham);
+//        adelaideCrows.addPlayer(pat);
+//        adelaideCrows.addPlayer(beckham);
 
         System.out.println(adelaideCrows.numPlayers());
+
+        Team<BaseballPlayer> baseballTeam = new Team<>("Chicago cubs");
+        baseballTeam.addPlayer(pat);
+
+
+        Team<SoccerPlayer> brokenTeam = new Team<>("manchester united");
+        brokenTeam.addPlayer(beckham);
+
+        Team<FootballPlayer> melbourne = new Team<>("Melbourne");
+        FootballPlayer banks = new FootballPlayer("Gordon");
+
+        Team<FootballPlayer> hawthorn = new Team<>("Hawthorn");
+        Team<FootballPlayer> fremantle = new Team<>("Fremantle");
+
+        hawthorn.matchResult(fremantle,1,0);
+        hawthorn.matchResult(adelaideCrows,3,8);
+
+        adelaideCrows.matchResult(fremantle,2,1);
+        //adelaideCrows.matchResult(baseballTeam,1,1);
+
 
     }
 
--- java practise projects/UdemyMasterClass/Generics/src/GenericsPart2/Team.java
+++ java practise projects/UdemyMasterClass/Generics/src/GenericsPart2/Team.java
@@ -2,7 +2,7 @@ package GenericsPart2;
 
 import java.util.ArrayList;
 
-public class Team {
+public class Team<T extends Player> {
 
     private String name;
     int played = 0;
@@ -10,7 +10,7 @@ public class Team {
     int lost = 0;
     int tied = 0;
 
-    private ArrayList<Player> members = new ArrayList<>();
+    private ArrayList<T> members = new ArrayList<>();
 
     public Team(String name) {
         this.name = name;
@@ -20,14 +20,14 @@ public class Team {
         return name;
     }
 
-    public boolean addPlayer(Player player){
+    public boolean addPlayer(T player){
         if(members.contains(player)){
-            System.out.println(player.getName() + " is already on this team.");
+            System.out.println( player.getName() + " is already on this team." );
             return false;
         }
         else{
             members.add(player);
-            System.out.println(player.getName() + "picked for team" + this.name);
+            System.out.println( player.getName() + "picked for team" + this.name );
             return true;
         }
     }
@@ -36,20 +36,26 @@ public class Team {
         return this.members.size();
     }
 
-    public void matchResult(Team opponent,int ourScore,int theirScore){
+    public void matchResult(Team<T> opponent,int ourScore,int theirScore){
+
+        String message;
 
         if(ourScore > theirScore){
             won++ ;
+            message = " beat ";
         }
         else if (ourScore == theirScore){
             tied++;
+            message = " drew with ";
         }
         else{
             lost++;
+            message = " lost to ";
         }
         played++;
 
         if(opponent != null){
+            System.out.println(this.getName() + message + opponent.getName());
             opponent.matchResult(null,theirScore,ourScore);
         }
     }
