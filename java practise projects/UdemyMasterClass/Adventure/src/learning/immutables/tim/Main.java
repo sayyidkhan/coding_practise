5e3f0133195fdc141b46c0f721c9bcad9f55a9fe
Sayyid Khan <sayyidkhan92@hotmail.com>
2019-04-25 22:55:30 +0800

    learning to work with immutable classes

--- java practise projects/UdemyMasterClass/Adventure/src/learning/immutables/tim/Main.java
+++ java practise projects/UdemyMasterClass/Adventure/src/learning/immutables/tim/Main.java
@@ -20,7 +20,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         Map<String, Integer> tempExit = new HashMap<String, Integer>();
-        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java",tempExit));
+        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java",null));
 
         // room 1
         tempExit = new HashMap<String, Integer>();
