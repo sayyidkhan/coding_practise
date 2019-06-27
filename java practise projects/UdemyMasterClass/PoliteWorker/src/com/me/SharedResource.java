973711033082f3532ddf13d079b01e4eda708791
Sayyid Khan <sayyidkhan92@hotmail.com>
2019-06-27 22:12:53 +0800

    learning javafx with concurrency

---
+++ java practise projects/UdemyMasterClass/PoliteWorker/src/com/me/SharedResource.java
@@ -0,0 +1,21 @@
+package com.me;
+
+
+
+public class SharedResource {
+
+    private Worker owner;
+
+    public SharedResource(Worker owner) {
+        this.owner = owner;
+    }
+
+    public Worker getOwner() {
+        return owner;
+    }
+
+    public synchronized void setOwner(Worker owner) {
+        this.owner = owner;
+    }
+
+}
