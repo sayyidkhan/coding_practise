b1a8de59b8920da5afe672c4bae9e492ef076256
Sayyid Khan <sayyidkhan92@hotmail.com>
2019-07-22 22:16:18 +0800

    Intro to lambda Expressions

---
+++ java practise projects/UdemyMasterClass/ButtonFX/src/module-info.java
@@ -0,0 +1,6 @@
+module sample {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+    opens sample;
+}
--- java practise projects/UdemyMasterClass/Lambdas_New/src/com/me/Main.java
+++ java practise projects/UdemyMasterClass/Lambdas_New/src/com/me/Main.java
@@ -1,5 +1,7 @@
 package com.me;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -47,8 +49,24 @@ public class Main {
             System.out.println(employee.getName());
         }
 
+//        String sillyString = doStringStuff(new UpperConcat() {
+//            @Override
+//            public String upperAndConcat(String s1, String s2) {
+//                return s1.toUpperCase() + s2.toUpperCase();
+//            }
+//        },
+//                employees.get(0).getName(),employees.get(1).getName());
+//        System.out.println(sillyString);
+
+        // lambda expression of it
+        UpperConcat uc = (s1,s2) -> s1.toUpperCase() + s2.toUpperCase();
+        String sillyString = doStringStuff(uc,employees.get(0).getName(),employees.get(0).getName());
+
     }
 
+    public final static String doStringStuff(@NotNull UpperConcat uc, String s1, String s2){
+        return uc.upperAndConcat(s1,s2);
+    }
 
 }
 
@@ -76,5 +94,9 @@ class Employee {
     public void setAge(int age) {
         this.age = age;
     }
+}
+
+interface UpperConcat {
+    public String upperAndConcat(String s1,String s2);
 }
 
