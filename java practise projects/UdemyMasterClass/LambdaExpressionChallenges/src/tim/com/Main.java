10ee595c4dc0bdeaa04c1ca856d3e126fac66c4f
Sayyid Khan <sayyidkhan92@hotmail.com>
2019-08-05 21:50:56 +0800

    doing the lambda expression challenge

---
+++ java practise projects/UdemyMasterClass/LambdaExpressionChallenges/src/tim/com/Main.java
@@ -0,0 +1,19 @@
+package tim.com;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // challenge 1
+//        Runnable task = () -> {
+//            String myString = "Let's split this up into an array";
+//            String[] parts = myString.split(" ");
+//            for(String part:parts){
+//                System.out.println(part);
+//            }
+//        };
+
+
+
+    }
+
+}
