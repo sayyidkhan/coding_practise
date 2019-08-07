e606ddacdfef0ca59babecbceac9cb4e92e53b51
Sayyid Khan <sayyidkhan92@hotmail.com>
2019-08-07 22:03:27 +0800

    doing the lambda expression challenge

--- java practise projects/UdemyMasterClass/LambdaExpressionChallenges/src/tim/com/Main.java
+++ java practise projects/UdemyMasterClass/LambdaExpressionChallenges/src/tim/com/Main.java
@@ -1,5 +1,9 @@
 package tim.com;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -12,8 +16,27 @@ public class Main {
 //            }
 //        };
 
+        // challenge 2
+        Function<String,String> lambdaFunction = s -> {
+            StringBuilder returnVal = new StringBuilder();
+            for(int i = 0; i < s.length(); i++){
+                if(i % 2 == 1){
+                    returnVal.append(s.charAt(i));
+                }
+            }
+            return returnVal.toString();
+        };
+
+        // challenge 3 - ans
+        System.out.println(lambdaFunction.apply("1234567890"));
 
 
+
+    }
+
+    // challenge 5
+    public static String everySecondChar(Function<String,String> func,String source){
+        return func.apply(source);
     }
 
 }
