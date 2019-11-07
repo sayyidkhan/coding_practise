2cbc0dccea694d662697dba663044c724b1bffb1
Sayyid Khan <sayyidkhan92@hotmail.com>
2019-11-07 22:41:06 +0800

    introduction to learning how to code in dart and flutter

--- flutter practise projects/udemy - academind/flutter_complete_guide/lib/main.dart
+++ flutter practise projects/udemy - academind/flutter_complete_guide/lib/main.dart
@@ -20,30 +20,47 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
 
+  final questions = const [
+    {
+      'questionText' : 'what\'s your favourite color?',
+      'answers' : ['Black','Red','Green','White'],
+    },
+    {
+      'questionText' : 'what\'s your favourite animal?',
+      'answers' : ['Rabiit','Snake','Elephant','Lion'],
+    },
+    {
+      'questionText' : 'what\'s your favourite instructor?',
+      'answers' : ['Max1','Max2','Max3','Max4'],
+    },
+  ];
+
   void _answerQuestion(){
     setState(() {
       _questionIndex += 1;
     });
     print(_questionIndex);
+    if (_questionIndex < questions.length) {
+      print("We Have more questions!");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
 
-    var questions = [
-      {
-        'questionText' : 'what\'s your favourite color?',
-        'answers' : ['Black','Red','Green','White'],
-      },
-      {
-        'questionText' : 'what\'s your favourite animal?',
-        'answers' : ['Rabiit','Snake','Elephant','Lion'],
-      },
-      {
-        'questionText' : 'what\'s your favourite instructor?',
-        'answers' : ['Max1','Max2','Max3','Max4'],
-      },
-      ];
+    var resultTrue =
+    Column(
+        children: [
+    Question( questions[_questionIndex]['questionText'] ),
+    ...(questions[_questionIndex]['answers'] as List<String>)
+        .map((answer) {
+    return Answer(_answerQuestion,answer);
+    }).toList()
+    ],);
+
+    var resultFalse =
+    Center(child: Text("You did it!"));
+
     return MaterialApp(
       home:
       Scaffold(
@@ -51,14 +68,7 @@ class _MyAppState extends State<MyApp> {
           title: Text("My First App"),
         ),
         body:
-        Column(
-          children: [
-            Question( questions[_questionIndex]['questionText'] ),
-            ...(questions[_questionIndex]['answers'] as List<String>)
-                .map((answer) {
-              return Answer(_answerQuestion,answer);
-            }).toList()
-          ],),
+        (_questionIndex < questions.length) ? resultTrue : resultFalse,
       ),
     );
   }
