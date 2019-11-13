f89c6c8697c5ff091eb17093164e88e77218e252
Sayyid Khan <sayyidkhan92@hotmail.com>
2019-11-14 00:21:53 +0800

    learning how to do an expense tracker app

--- java practise projects/UdemyMasterClass/expense_tracker/lib/main.dart
+++ java practise projects/UdemyMasterClass/expense_tracker/lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:expense_tracker/widgets/new_transaction.dart';
+import 'package:expense_tracker/widgets/transaction_list.dart';
+import 'package:expense_tracker/widgets/user_transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import './transaction.dart';
+import 'package:expense_tracker/models/transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,11 +18,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(id: "t1",title: "new Shoes",amount: 69.99,date: DateTime.now()),
-    Transaction(id: "t2",title: "weekly groceries",amount: 16.53,date: DateTime.now()),
-  ];
-
 
 //  String titleInput;
 //  String amountInput;
@@ -47,63 +45,9 @@ class MyHomePage extends StatelessWidget {
               color: Colors.blue,
             )),
 
-            Card(
-              elevation: 5,
-              child: Container(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: <Widget>[
-                    TextField(
-                      decoration: InputDecoration(labelText: 'Title'),
-                      controller: titleController,
-//                      onChanged: (val) => titleInput = val ,
-                    ),
-                    TextField(
-                      decoration: InputDecoration(labelText: 'Amount'),
-                      controller: amountController,
-//                      onChanged: (val) => amountInput = val ,
-                    ),
-                    FlatButton(
-                      child: Text('Add Transaction'),
-                      textColor: Colors.purple,
-                      onPressed: () {
-//                        print(titleInput);
-//                        print(amountInput);
-                          print(titleController.text);
-                          print(amountController.text);
-                      },
-                    ),
-                  ],
-                ),
-              ),
-            ),
-
+            UserTransactions()
             //card is to hold the list of transaction
-            Column(
-              children: transactions.map((tx) {
-                return Card(
-                    child: Row(children: <Widget>[
 
-                      Container(
-                        child:
-                        Text("\$${tx.amount}" ,style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20,color: Colors.purple,),),
-                        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                        decoration: BoxDecoration(border: Border.all(color: Colors.purple,width: 2),),
-                        padding: EdgeInsets.all(10),
-                      ),
-
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                        Text(tx.title,style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold)),
-                        Text( DateFormat.yMMMd().format(tx.date) ,style: TextStyle(color: Colors.grey),),
-                      ],
-                      ),
-
-                    ],)
-                );
-              }).toList(),),
           ],
         ),
     );
