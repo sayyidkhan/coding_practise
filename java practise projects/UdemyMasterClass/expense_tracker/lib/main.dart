b996f9ca7e956666b8903b2f4cbd8f418b494634
Sayyid Khan <sayyidkhan92@hotmail.com>
2019-11-17 14:57:51 +0800

    finish building the first app in flutter

--- java practise projects/UdemyMasterClass/expense_tracker/lib/main.dart
+++ java practise projects/UdemyMasterClass/expense_tracker/lib/main.dart
@@ -15,21 +15,23 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
         accentColor: Colors.amber,
+        errorColor: Colors.red,
         fontFamily: 'Quicksand',
         textTheme: ThemeData.light().textTheme.copyWith(
-            title: TextStyle(
-            fontFamily: 'OpenSans',
-              fontWeight: FontWeight.bold,
-              fontSize: 18
-            )
+              title: TextStyle(
+              fontFamily: 'OpenSans',
+                fontWeight: FontWeight.bold,
+                fontSize: 18
+              ),
+              button: TextStyle(color: Colors.white),
         ),
         appBarTheme: AppBarTheme(
           textTheme: ThemeData.light().textTheme.copyWith(
-            title: TextStyle(
-                fontFamily: 'OpenSans',
-                fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
+              title: TextStyle(
+                  fontFamily: 'OpenSans',
+                  fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
           ),
         ),
       ),
@@ -67,11 +69,11 @@ class _MyHomePageState extends State<MyHomePage> {
     }).toList();
   }
 
-  void _addNewTransaction(String txTitle, double txAmount) {
+  void _addNewTransaction(String txTitle, double txAmount,DateTime chosenDate) {
     final newTx = Transaction(
       title: txTitle,
       amount: txAmount,
-      date: DateTime.now(),
+      date: chosenDate,
       id: DateTime.now().toString(),
     );
 
@@ -93,6 +95,12 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  void _deleteTransaction(String id) {
+      setState(() {
+        _userTransactions.removeWhere((tx) => (tx.id == id) );
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -111,7 +119,7 @@ class _MyHomePageState extends State<MyHomePage> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Chart(_recentTransactions),
-            TransactionList(_userTransactions),
+            TransactionList(_userTransactions, _deleteTransaction),
           ],
         ),
       ),
