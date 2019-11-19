17a7209a9129e40c536b1fd7c1c374b34f3fb554
Sayyid Khan <sayyidkhan92@hotmail.com>
2019-11-19 23:31:26 +0800

    learning how to build a flutter app in the second part of the course

--- java practise projects/UdemyMasterClass/expense_tracker/lib/main.dart
+++ java practise projects/UdemyMasterClass/expense_tracker/lib/main.dart
@@ -114,8 +114,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-
-    final isLandscape =  MediaQuery.of(context).orientation == Orientation.landscape;
+    final mediaQuery = MediaQuery.of(context);
+    final isLandscape =  mediaQuery.orientation == Orientation.landscape;
 
     final appBar = AppBar(
       title: Text('Personal Expenses', style: TextStyle(fontFamily: 'OpenSans'),),
@@ -128,9 +128,9 @@ class _MyHomePageState extends State<MyHomePage> {
     );
 
     final txListWidget = Container(                                                        
-        height: (MediaQuery.of(context).size.height
+        height: (mediaQuery.size.height
                 - appBar.preferredSize.height
-                - MediaQuery.of(context).padding.top)
+                - mediaQuery.padding.top)
                 * 0.7,
         child: TransactionList(_userTransactions, _deleteTransaction));
 
@@ -153,9 +153,9 @@ class _MyHomePageState extends State<MyHomePage> {
             ],),
             if(!isLandscape)
             Container(
-                height: (MediaQuery.of(context).size.height
+                height: (mediaQuery.size.height
                          - appBar.preferredSize.height
-                         - MediaQuery.of(context).padding.top)
+                         - mediaQuery.padding.top)
                          * 0.3,
                 child:  Chart(_recentTransactions) ),
            if(!isLandscape)
