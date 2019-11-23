50fbfa4cdef3f5ccdab6c76a6bc5cdb522d6994b
Sayyid Khan <sayyidkhan92@hotmail.com>
2019-11-23 15:37:05 +0800

    learning how to use constant constructors in dart and flutter

--- java practise projects/UdemyMasterClass/expense_tracker/lib/main.dart
+++ java practise projects/UdemyMasterClass/expense_tracker/lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 import './widgets/new_transaction.dart';
 import './widgets/transaction_list.dart';
@@ -116,6 +115,60 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  List<Widget> _buildLandscapeContent(
+      MediaQueryData mediaQueryData,
+      AppBar appBar,
+      Widget txListWidget
+      ) {
+    return
+      [Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          'Show Chart',
+          style: Theme.of(context).textTheme.title,
+        ),
+        Switch.adaptive(
+          activeColor: Theme.of(context).accentColor,
+          value: _showChart,
+          onChanged: (val) {
+            setState(() {
+              _showChart = val;
+            });
+          },
+        ),
+      ],
+      ),
+      _showChart
+          ? Container(
+        height: (mediaQueryData.size.height -
+            appBar.preferredSize.height -
+            mediaQueryData.padding.top) *
+            0.7,
+        child: Chart(_recentTransactions),
+      )
+          : txListWidget
+      ];
+  }
+
+  List<Widget> _buildPortraitContent(
+      MediaQueryData mediaQueryData,
+      AppBar appBar,
+      Widget txListWidget
+      ) {
+    return
+      [ Container(
+      height: (mediaQueryData.size.height -
+          appBar.preferredSize.height -
+          mediaQueryData.padding.top) *
+          0.3,
+      child: Chart(_recentTransactions),
+      ),
+      txListWidget];
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
@@ -160,63 +213,37 @@ class _MyHomePageState extends State<MyHomePage> {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
               if (isLandscape)
-          Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Show Chart',
-              style: Theme.of(context).textTheme.title,
-            ),
-            Switch.adaptive(
-              activeColor: Theme.of(context).accentColor,
-              value: _showChart,
-              onChanged: (val) {
-                setState(() {
-                  _showChart = val;
-                });
-              },
-            ),
-          ],
-        ),
-        if (!isLandscape)
-    Container(
-      height: (mediaQuery.size.height -
-          appBar.preferredSize.height -
-          mediaQuery.padding.top) *
-          0.3,
-      child: Chart(_recentTransactions),
-    ),
-    if (!isLandscape) txListWidget,
-    if (isLandscape)
-    _showChart
-    ? Container(
-    height: (mediaQuery.size.height -
-    appBar.preferredSize.height -
-    mediaQuery.padding.top) *
-    0.7,
-    child: Chart(_recentTransactions),
-    )
-        : txListWidget
-    ],
-    ),
-    ),
-    );
-    return Platform.isIOS
-    ? CupertinoPageScaffold(
-    child: pageBody,
-    navigationBar: appBar,
-    )
-        : Scaffold(
-    appBar: appBar,
-    body: pageBody,
-    floatingActionButtonLocation:
-    FloatingActionButtonLocation.centerFloat,
-    floatingActionButton: Platform.isIOS
-    ? Container()
-        : FloatingActionButton(
-    child: Icon(Icons.add),
-    onPressed: () => _startAddNewTransaction(context),
-    ),
-    );
+                ..._buildLandscapeContent(
+                    mediaQuery,
+                    appBar,
+                    txListWidget
+                ),
+              if (!isLandscape)
+                ..._buildPortraitContent(
+                    mediaQuery,
+                    appBar,
+                    txListWidget
+                ),
+              ],
+              ),
+              ),
+              );
+              return Platform.isIOS
+              ? CupertinoPageScaffold(
+              child: pageBody,
+              navigationBar: appBar,
+              )
+                  : Scaffold(
+              appBar: appBar,
+              body: pageBody,
+              floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerFloat,
+              floatingActionButton: Platform.isIOS
+              ? Container()
+                  : FloatingActionButton(
+              child: Icon(Icons.add),
+              onPressed: () => _startAddNewTransaction(context),
+              ),
+              );
   }
 }
