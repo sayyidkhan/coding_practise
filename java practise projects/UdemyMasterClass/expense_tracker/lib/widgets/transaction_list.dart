85e6b3388a4d42aae578c2939520fb15b8f525f3
Sayyid Khan <sayyidkhan92@hotmail.com>
2019-11-23 21:16:10 +0800

    finish learning widget and flutter internals - deep dive

--- java practise projects/UdemyMasterClass/expense_tracker/lib/widgets/transaction_list.dart
+++ java practise projects/UdemyMasterClass/expense_tracker/lib/widgets/transaction_list.dart
@@ -14,32 +14,30 @@ class TransactionList extends StatelessWidget {
     print('build() TransactionList');
     return transactions.isEmpty
         ? LayoutBuilder(builder: (ctx, constraints) {
-            return Column(
-              children: <Widget>[
-                Text(
-                  'No transactions added yet!',
-                  style: Theme.of(context).textTheme.title,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Container(
-                    height: constraints.maxHeight * 0.6,
-                    child: Image.asset(
-                      'assets/images/waiting.png',
-                      fit: BoxFit.cover,
-                    )),
-              ],
-            );
-          })
-        : ListView.builder(
-            itemBuilder: (ctx, index) {
-              return TransactionItem(
-                transaction: transactions[index],
-                deleteTx: deleteTx,
-              );
-            },
-            itemCount: transactions.length,
-          );
+      return Column(
+        children: <Widget>[
+          Text(
+            'No transactions added yet!',
+            style: Theme.of(context).textTheme.title,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+              height: constraints.maxHeight * 0.6,
+              child: Image.asset(
+                'assets/images/waiting.png',
+                fit: BoxFit.cover,
+              )),
+        ],
+      );
+    })
+        : ListView(
+      children: transactions.map((tx) => TransactionItem(
+        key: ValueKey(tx.id),
+        transaction: tx,
+        deleteTx: deleteTx,
+      )).toList(),
+    );
   }
 }
