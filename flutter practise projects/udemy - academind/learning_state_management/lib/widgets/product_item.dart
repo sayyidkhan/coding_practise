91894c9f540c75615bc225e3fa7393dec2229d3c
Sayyid Khan <sayyidkhan92@hotmail.com>
2019-12-11 23:07:45 +0800

    learning how to build a shop app

--- flutter practise projects/udemy - academind/learning_state_management/lib/widgets/product_item.dart
+++ flutter practise projects/udemy - academind/learning_state_management/lib/widgets/product_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:learning_state_management/screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
   final String id;
@@ -10,25 +11,38 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(
-        imageUrl,
-        fit: BoxFit.cover,
-      ),
-      footer: GridTileBar(
-        backgroundColor: Colors.black54,
-        leading: IconButton(
-            icon: Icon(Icons.favorite),
-            onPressed: () {},
+    //clipRoundedRectangle
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(
+            ProductDetailScreen.routeName,arguments: id,
+            );
+          },
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
         ),
-        title: Text(
-          title,
-          textAlign: TextAlign.center,
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+              icon: Icon(Icons.favorite),
+              color: Theme.of(context).accentColor,
+              onPressed: () {},
+          ),
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: () {},
+              color: Theme.of(context).accentColor,
+              ),
         ),
-        trailing: IconButton(
-            icon: Icon(Icons.shopping_cart),
-            onPressed: () {}
-            ),
       ),
     );
   }
