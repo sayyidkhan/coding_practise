64a3f2b39777882c56c90fc35bef1b002d2abdb8
Sayyid Khan <sayyidkhan92@hotmail.com>
2019-12-25 17:38:11 +0800

    learning how to work with forms in flutter and learning how to use FocusNode()

---
+++ flutter practise projects/udemy - academind/learning_state_management/lib/screens/edit_product_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class EditProductsScreen extends StatefulWidget {
+  static const routeName = "/edit-product";
+
+  @override
+  _EditProductsScreenState createState() => _EditProductsScreenState();
+}
+
+class _EditProductsScreenState extends State<EditProductsScreen> {
+  final _priceFocusNode = FocusNode();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Edit Product"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          child: ListView(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: "Title"),
+                textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: "Price"),
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.number,
+                focusNode: _priceFocusNode,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- flutter practise projects/udemy - academind/learning_state_management/lib/widgets/cart_item.dart
+++ flutter practise projects/udemy - academind/learning_state_management/lib/widgets/cart_item.dart
@@ -9,7 +9,7 @@ class CartItem extends StatelessWidget {
   final int quantity;
   final String title;
 
-  CartItem(this.id,this.productId, this.price, this.quantity, this.title);
+  CartItem(this.id, this.productId, this.price, this.quantity, this.title);
 
   @override
   Widget build(BuildContext context) {
@@ -17,12 +17,35 @@ class CartItem extends StatelessWidget {
       key: ValueKey(id),
       background: Container(
         color: Theme.of(context).errorColor,
-        child: Icon(Icons.delete,color: Colors.white,size: 40,),
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
         alignment: Alignment.centerRight,
         padding: EdgeInsets.only(right: 20),
         margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
       ),
       direction: DismissDirection.endToStart,
+      confirmDismiss: (direction) {
+        return showDialog(
+            context: context,
+            builder: (ctx) => AlertDialog(
+                  title: Text("Are you sure?"),
+                  content:
+                      Text("Do you want to remove the item from the cart?"),
+                  actions: <Widget> [
+                    FlatButton(child: Text("No"), onPressed: () {
+                      Navigator.of(ctx).pop(false);
+                    },),
+                    FlatButton(child: Text("Yes"), onPressed: () {
+                      Navigator.of(ctx).pop(true);
+                    },),
+                  ],
+                ),
+        );
+
+      },
       onDismissed: (direction) {
         Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
