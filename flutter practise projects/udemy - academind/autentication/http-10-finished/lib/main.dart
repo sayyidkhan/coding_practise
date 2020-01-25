b768b5ed884d8448203cd5abc12e4b20c6519591
Sayyid Khan <sayyidkhan92@hotmail.com>
2020-01-25 18:42:09 +0800

    learning how to add the signup page into flutter and learning how to work with the http post for validation of user autentication.

--- flutter practise projects/udemy - academind/autentication/http-10-finished/lib/main.dart
+++ flutter practise projects/udemy - academind/autentication/http-10-finished/lib/main.dart
@@ -33,14 +33,16 @@ class MyApp extends StatelessWidget {
           value: Orders(),
         ),
       ],
-      child: MaterialApp(
+      child:
+      Consumer<Auth>(builder: (ctx, auth, _) =>
+        MaterialApp(
           title: 'MyShop',
           theme: ThemeData(
             primarySwatch: Colors.purple,
             accentColor: Colors.deepOrange,
             fontFamily: 'Lato',
           ),
-          home: AuthScreen(),
+          home: auth.isAuth ? ProductsOverviewScreen : AuthScreen(),
           routes: {
             ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
             CartScreen.routeName: (ctx) => CartScreen(),
@@ -48,6 +50,8 @@ class MyApp extends StatelessWidget {
             UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
             EditProductScreen.routeName: (ctx) => EditProductScreen(),
           }),
+      )
+
     );
   }
 }
--- flutter practise projects/udemy - academind/autentication/http-10-finished/lib/providers/auth.dart
+++ flutter practise projects/udemy - academind/autentication/http-10-finished/lib/providers/auth.dart
@@ -2,6 +2,8 @@ import 'dart:convert';
 import 'package:flutter/widgets.dart';
 import  'package:http/http.dart' as http;
 
+import '../models/http_exception.dart';
+
 class Auth with ChangeNotifier {
   static const API_KEY = "AIzaSyBhKVbxyAPiOYSthzfAhsNs_9U-jSl-t_k";
 
@@ -10,20 +12,50 @@ class Auth with ChangeNotifier {
   DateTime _expiryDate;
   String _userId;
 
+  bool get isAuth {
+    return token != null;
+  }
+
+  String get token {
+    if(_expiryDate != null &&
+        _expiryDate.isAfter(DateTime.now()) &&
+       _token != null
+    ){
+      return _token;
+    }
+    return null;
+  }
+
   Future<void> _autenticate(String email, String password,String urlSegment) async {
     final url = 'https://identitytoolkit.googleapis.com/v1/accounts:' + urlSegment + '?key=' + API_KEY;
 
-    final response = await http.post(
-      url,
-      body: json.encode(
-        {
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        },
-      ),
-    );
-    print(json.decode(response.body));
+    try{
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      final responseData = json.decode(response.body);
+      if(responseData['error'] != null){
+        throw HttpException(responseData['error']['message']);
+      }
+      _token = responseData['idToken'];
+      _userId = responseData['localId'];
+      _expiryDate = DateTime.now().add(Duration(seconds: int.parse(responseData['expiresIn'])
+      ));
+      notifyListeners();
+      print(json.decode(response.body));
+    }
+    catch(error) {
+      throw error;
+    }
+
+
   }
 
   Future<void> signUp(String email, String password) async {
