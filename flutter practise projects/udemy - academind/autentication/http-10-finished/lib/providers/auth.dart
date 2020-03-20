d372288cf242fffe6ecc67f2c9e9d268cea22732
sayyidkhan <sayyidkhan92@hotmail.com>
2020-03-20 08:46:53 +0800

    learning how to build the auto-logout feature.

--- flutter practise projects/udemy - academind/autentication/http-10-finished/lib/providers/auth.dart
+++ flutter practise projects/udemy - academind/autentication/http-10-finished/lib/providers/auth.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:async';
 import 'package:flutter/widgets.dart';
 import  'package:http/http.dart' as http;
 
@@ -11,6 +12,7 @@ class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
+  Timer _authTimer;
 
   bool get isAuth {
     return token != null;
@@ -52,6 +54,7 @@ class Auth with ChangeNotifier {
       _userId = responseData['localId'];
       _expiryDate = DateTime.now().add(Duration(seconds: int.parse(responseData['expiresIn'])
       ));
+      _autoLogout();
       notifyListeners();
       print(json.decode(response.body));
     }
@@ -77,7 +80,21 @@ class Auth with ChangeNotifier {
     _token = null;
     _userId = null;
     _expiryDate = null;
+    if(_authTimer != null){
+      _authTimer.cancel();
+      _authTimer = null;
+    }
     notifyListeners();
+  }
+
+  void _autoLogout() {
+    if(_authTimer != null){
+      //cancel existing timers if available
+      _authTimer.cancel();
+    }
+    final timeNow = DateTime.now();
+    final timeToExpiry = _expiryDate.difference(timeNow).inSeconds;
+    _authTimer = Timer(Duration(seconds: timeToExpiry), logout);
   }
 
 }
