8a058ac84a4e368fc4a50dca062b604b7a388538
Sayyid Khan <sayyidkhan92@hotmail.com>
2020-03-31 11:06:39 +0800

    Update test.dart
    patch update

--- flutter practise projects/udemy - academind/flutter_frontend_nodejs_backend/flutter_code/lib/helpers/test.dart
+++ flutter practise projects/udemy - academind/flutter_frontend_nodejs_backend/flutter_code/lib/helpers/test.dart
@@ -3,8 +3,8 @@ import 'signing.dart';
 void main() {
 
   var result = new Signing(
-      "https://maps.googleapis.com/maps/api/staticmap?center=1.3805267,103.749145&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C1.3805267,103.749145&key=AIzaSyCLwyea3K_qUa5NHjHK_g_40sDvKX7JZwE",
-      "b5YBLHLguIOWB7TxtwqRq1I75_s=")
+      "",
+      "")
       .theResult;
   print(result);
 }
