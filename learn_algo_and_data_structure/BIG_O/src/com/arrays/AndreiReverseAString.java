3d54aec9f7a34c82942feec128489b290677a7d4
sayyidkhan <sayyidkhan92@hotmail.com>
2020-06-21 16:00:31 +0800

    learning how to do merge sorted arrays

---
+++ learn_algo_and_data_structure/BIG_O/src/com/arrays/AndreiReverseAString.java
@@ -0,0 +1,25 @@
+package com.arrays;
+
+public class AndreiReverseAString {
+
+    public static void main(String[] args) {
+        System.out.println(reverseString("Hi i am jack"));
+    }
+
+    private static String reverseString(String value){
+        if(value.length() == 1){
+            return value;
+        }
+        else {
+            String[] currentString = value.split("");
+            String[] reversedString = new String[value.length()];
+            int j = 0;
+            for(int i = value.length() - 1;i >= 0;i--){
+                reversedString[j] = currentString[i];
+                j++;
+            }
+            return String.join("",reversedString);
+        }
+    }
+
+}
