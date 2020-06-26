80b001092f8ebf4314cf8e96dc220798274eb014
sayyidkhan <sayyidkhan92@hotmail.com>
2020-06-26 15:23:58 +0800

    finished learning queue and stacks

---
+++ learn_algo_and_data_structure/BIG_O/src/stackAndQueue/stack/array/StackImplArrayForm.java
@@ -0,0 +1,65 @@
+package stackAndQueue.stack.array;
+
+
+import java.util.ArrayList;
+
+public class StackImplArrayForm {
+
+    public static void main(String[] args) {
+        StackImplArray sa = new StackImplArray();
+        sa.push(2);
+        sa.push(3);
+        sa.push(4);
+
+        sa.pop();
+        System.out.println("peeking: " + sa.peek());
+        sa.printList();
+    }
+
+}
+
+class StackImplArray {
+    private ArrayList<StackNode> stackNodes;
+
+    public StackImplArray() {
+        this.stackNodes = new ArrayList<>();
+    }
+
+    public int peek(){
+        return stackNodes.get(0).getValue();
+    }
+
+    public void push(int value){
+        stackNodes.add(0,new StackNode(value));
+    }
+
+    public void pop(){
+        stackNodes.remove(0);
+    }
+
+    public void printList(){
+        stackNodes.forEach((item) -> {
+            System.out.println(item.getValue());
+        });
+    }
+
+}
+
+class StackNode {
+    private int value;
+
+    public StackNode(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+}
+
+
---
+++ learn_algo_and_data_structure/BIG_O/src/stackAndQueue/stack/linkedlist/StackImplLinkedList.java
@@ -0,0 +1,111 @@
+package stackAndQueue.stack.linkedlist;
+
+
+public class StackImplLinkedList {
+
+    public static void main(String[] args) {
+        LinkedListStack ls = new LinkedListStack();
+        ls.push(2);
+        ls.push(3);
+        ls.push(4);
+
+        int peek = ls.peek();
+        System.out.println("currently peeking:" + peek);
+        ls.pop();
+        ls.pop();
+
+        ls.printLinkedList();
+    }
+
+}
+
+class LinkedListStack {
+    public LinkedListStackNode top;
+    public LinkedListStackNode bottom;
+    private int length;
+
+    public LinkedListStack() {
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
+
+    public Integer peek() {
+        if(isEmpty()){
+            System.out.println("stack is empty");
+            return null;
+        }
+        else{
+            return this.top.getValue();
+        }
+    }
+
+    public void push(int value) {
+        if(isEmpty()){
+            this.top = new LinkedListStackNode(value);
+            this.bottom = this.top;
+        }
+        else{
+            LinkedListStackNode newNode = new LinkedListStackNode(value);
+            newNode.setNext(this.top);
+            this.top = newNode;
+        }
+        this.length++;
+    }
+
+    public void pop() {
+        //assign to null when there is nothing else to remove
+        if(this.length == 1){
+            this.bottom = null;
+        }
+
+        if(isEmpty()){
+            System.out.println("nothing to pop");
+        }
+        else{
+            LinkedListStackNode nextNode = this.top.getNext();
+            this.top = nextNode;
+        }
+        this.length--;
+    }
+
+    public void printLinkedList(){
+        LinkedListStackNode currentNode = this.top;
+        while (currentNode != null){
+            System.out.println(currentNode.getValue());
+            currentNode = currentNode.getNext();
+        }
+    }
+
+    public boolean isEmpty(){
+        return this.length == 0;
+    }
+
+
+}
+
+class LinkedListStackNode {
+    private int value;
+    private LinkedListStackNode next;
+
+    public LinkedListStackNode(int value) {
+        this.value = value;
+        this.next = null;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public LinkedListStackNode getNext() {
+        return next;
+    }
+
+    public void setNext(LinkedListStackNode next) {
+        this.next = next;
+    }
+}
